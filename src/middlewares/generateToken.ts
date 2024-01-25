11d5d0460a419e91a350fbcd591d734510e7c4d4
santhosh210 <kondasanthosh210@gmail.com>
2024-01-25 16:56:45 +0530

    removed unused declarations

--- src/middlewares/generateToken.ts
+++ src/middlewares/generateToken.ts
@@ -1,10 +1,6 @@
 import * as jwt from "jsonwebtoken";
 
 export async function generateToken(name: String, role: String) {
-  let token = jwt.sign(
-    { name: name, role: role },
-    "AIzaSyBtGuIOhOR6D1difgbeaPQtLtHYwwGe5wM",
-    { expiresIn: "1h" }
-  );
+  let token = jwt.sign({ name: name, role: role }, "", { expiresIn: "1h" });
   return token;
 }
